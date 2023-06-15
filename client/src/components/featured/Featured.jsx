97eaccb130014bc113401aa5155784df427f357a
Aditya Sharma <adityawavnew@gmail.com>
2023-06-15 10:26:31 +0530

    displayed the featured item by fetching from api

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,50 +1,70 @@
 import { InfoOutlined, PlayArrow } from "@mui/icons-material"
 import "./featured.scss"
+import { useEffect,useState } from "react";
+import axios from"axios";
 
 function Featured({type}) {
-  return (
-    <div className="featured">
-        {type && (
-            <div className="category">
-                <span>{type === "movies" ? "Movies" : "Series"}</span>
-                <select name="genre" id="genre">
-                    <option>Genre</option>
-                    <option value="adventure">Adventure</option>
-                    <option value="comedy">Comedy</option>
-                    <option value="crime">Crime</option>
-                    <option value="fantasy">Fantasy</option>
-                    <option value="historical">Historical</option>
-                    <option value="horror">Horror</option>
-                    <option value="romance">Romance</option>
-                    <option value="sci-fi">Sci-fi</option>
-                    <option value="thriller">Thriller</option>
-                    <option value="western">Western</option>
-                    <option value="animation">Animation</option>
-                    <option value="drama">Drama</option>
-                    <option value="documentary">Documentary</option>
-                </select>
-            </div>
-        )}
-        
-        <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg" alt="" />
-        <div className="info">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/1/15/Logonfx.png" alt="" />
-            <span className="desc">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum doloremque nesciunt cum tenetur odit! Recusandae, sequi accusantium. Saepe facilis corporis labore voluptatum et minus nam doloremque nulla accusantium, magnam impedit?
-            </span>
-            <div className="buttons">
-                <button className="play">
-                    <PlayArrow/>
-                    <span>Play</span>
+    const [content, setContent] = useState({});
+
+    useEffect(()=> {
+        const getRandomContent = async ()=>{
+            try {
+                const res = await axios.get(`/movies/random?type=${type}`, {
+                headers: {
+                    token:
+                      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODZkMTkzZjkxNjU0NzJjMGFjNGEyZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4NjczMTM0MiwiZXhwIjoxNjg3MTYzMzQyfQ.qeEcwKI2h4hPerFb-rHrGvzS0R25ewL5mrorH_1-D2w"
+                    },
+                });
+                setContent(res.data[0]);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getRandomContent();
+    }, [type])
+    return (
+        <div className="featured">
+            {type && (
+                <div className="category">
+                    <span>{type === "movies" ? "Movies" : "Series"}</span>
+                    <select name="genre" id="genre">
+                        <option>Genre</option>
+                        <option value="adventure">Adventure</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantasy">Fantasy</option>
+                        <option value="historical">Historical</option>
+                        <option value="horror">Horror</option>
+                        <option value="romance">Romance</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Western</option>
+                        <option value="animation">Animation</option>
+                        <option value="drama">Drama</option>
+                        <option value="documentary">Documentary</option>
+                    </select>
+                </div>
+            )}
+            
+            <img src={content.img} alt="" />
+            <div className="info">
+                <img src={content.imgTitle} alt="" />
+                <span className="desc">
+                    {content.desc}
+                </span>
+                <div className="buttons">
+                    <button className="play">
+                        <PlayArrow/>
+                        <span>Play</span>
+                        </button>
+                    <button className="more">
+                        <InfoOutlined/>
+                        <span>Info</span>
                     </button>
-                <button className="more">
-                    <InfoOutlined/>
-                    <span>Info</span>
-                </button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default Featured
+export default Featured;
