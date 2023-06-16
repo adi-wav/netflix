d7baf49e0fe404cb1e86eeaedced80c8296164fa
Aditya Sharma <adityawavnew@gmail.com>
2023-06-16 23:40:50 +0530

    made sidebar component of admin panel

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -21,49 +21,59 @@ export default function Sidebar() {
                     Sales
                 </li>
             </ul>
+        </div>
+        <div className="sidebarMenu">
             <h3 className="sidebarTitle">Quick Menu</h3>
             <ul className="sidebarList">
                 <li className="sidebarListItem">
-                    <LineStyle/>
-                    Home
+                    <LineStyle className="sidebarIcon"/>
+                    Users
                 </li>
                 <li className="sidebarListItem">
-                    <Timeline/>
-                    Analytics
+                    <Timeline className="sidebarIcon"/>
+                    Products
                 </li>
                 <li className="sidebarListItem">
-                    <TrendingUp/>
-                    Sales
+                    <TrendingUp className="sidebarIcon"/>
+                    Transactions
+                </li>
+                <li className="sidebarListItem">
+                    <TrendingUp className="sidebarIcon"/>
+                    Reports
                 </li>
             </ul>
+        </div>
+        <div className="sidebarMenu">
             <h3 className="sidebarTitle">Notifications</h3>
             <ul className="sidebarList">
                 <li className="sidebarListItem">
-                    <LineStyle/>
-                    Home
+                    <LineStyle className="sidebarIcon"/>
+                    Mail
                 </li>
                 <li className="sidebarListItem">
-                    <Timeline/>
-                    Analytics
+                    <Timeline className="sidebarIcon"/>
+                    Feedback
                 </li>
                 <li className="sidebarListItem">
-                    <TrendingUp/>
-                    Sales
+                    <TrendingUp className="sidebarIcon"/>
+                    Messages
                 </li>
             </ul>
+        </div>
+        <div className="sidebarMenu">
             <h3 className="sidebarTitle">Staff</h3>
             <ul className="sidebarList">
                 <li className="sidebarListItem">
-                    <LineStyle/>
-                    Home
+                    <LineStyle className="sidebarIcon"/>
+                    Manage
                 </li>
                 <li className="sidebarListItem">
-                    <Timeline/>
+                    <Timeline className="sidebarIcon"/>
                     Analytics
                 </li>
                 <li className="sidebarListItem">
-                    <TrendingUp/>
-                    Sales
+                    <TrendingUp className="sidebarIcon"/>
+                    Reports
                 </li>
             </ul>
         </div>
